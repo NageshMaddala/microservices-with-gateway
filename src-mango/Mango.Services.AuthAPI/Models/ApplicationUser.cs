be0f9dd6449149b6a695771c87c48db4e3a9b437
NageshMaddala <nageshvmr@gmail.com>
2023-07-27 16:23:20 +0530

    Added migration for aspnet identity, also created ApplicationUser table

---
+++ src-mango/Mango.Services.AuthAPI/Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Models
+{
+    /// <summary>
+    /// This is how IdentityUser is extended
+    /// Post that we need to ApplicationUser in DBContext
+    /// and also in program.cs relatedt to AddIdentity
+    /// </summary>
+    /// <remarks>
+    /// Here in this case ApplicationUser table won't be created
+    /// instead columns specified in ApplicationUser would be added to AspNetUsers table
+    /// This is by design of aspnet identity
+    /// </remarks>    
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+    }
+}
--- src-mango/Mango.Services.AuthAPI/Program.cs
+++ src-mango/Mango.Services.AuthAPI/Program.cs
@@ -1,6 +1,28 @@
+/*
+ * Brief Note: The net identity is a package that will create all the identity
+ * related tables automatically with entity framework core. That requires
+ * setting up entity framework, core and dbcontext.
+ */
+
+using Mango.Services.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// setup identity
+// 
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+       .AddEntityFrameworkStores<AppDbContext>()
+       .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,8 +40,29 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Add authentication to pipeline
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
 
+ApplyMigration();
+
 app.Run();
+
+void ApplyMigration()
+{
+    // Below will automatically apply pending migrations
+    // add-migration AddCouponToDb, update-database, add-migration SeedCouponTables
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
